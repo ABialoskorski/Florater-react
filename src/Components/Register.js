0f2171b13361d291316abc5d608228894843ae33
Bialoskorski <artur.bialoskorski@capgemini.com>
2019-07-22 15:40:28 +0200

    deleted not necesessary stuff from vue

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,51 +1,71 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./Register.css";
 
 class Register extends Component {
 	state = {};
-	handleRegister = () => {};
+
+	handleRegister = () => {
+		const link = "http://127.0.0.1:8000/";
+		const axios = require("axios");
+
+		const Register = () => {
+			try {
+				return axios.post(`${link}users/register/`, {
+					first_name: this.first_name,
+					last_name: this.last_name,
+					password: this.password,
+					email: this.email,
+				});
+			} catch (error) {
+				alert(error);
+			}
+		};
+
+		const showRegistrationResponse = async () => {
+			const data = Register()
+				.then(response => {
+					alert("Zalogowano pomyślnie");
+				})
+				.catch(error => {
+					alert(
+						"Rejestracja nie powiodła się, wypełnij poprawnie formularz"
+					);
+				});
+		};
+		showRegistrationResponse();
+	};
 	render() {
 		return (
 			<div className="register">
 				<div className="register__inputCollection">
 					<input
 						className="register__input"
-						v-validate="'required'"
 						name="first_name"
 						type="text"
 						label="first_name"
 						placeholder="Imię"
-						v-model="first_name"
 					/>
-					<v-spacer />
 					<input
 						className="register__input"
-						v-validate="'required'"
 						name="last_name"
 						type="text"
 						label="last_name"
 						placeholder="Nazwisko"
-						v-model="last_name"
 					/>
-					<v-spacer />
 					<input
 						className="register__input"
-						v-validate="'required|email'"
 						name="email"
 						type="text"
 						label="email"
 						placeholder="E-Mail"
-						v-model="email"
 					/>
-					<v-spacer />
 					<input
 						className="register__input"
-						v-validate="'required|password'"
 						name="password"
 						type="password"
 						label="password"
 						placeholder="Hasło"
-						v-model="password"
 					/>
 				</div>
 				<div className="register__buttons">
