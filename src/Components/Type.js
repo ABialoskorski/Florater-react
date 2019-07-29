9e3671cf05e330789d878681601fe908deb91786
Bialoskorski <artur.bialoskorski@capgemini.com>
2019-07-29 15:32:41 +0200

    mapped through response

--- src/Components/Type.js
+++ src/Components/Type.js
@@ -5,34 +5,38 @@ import "./Type.css";
 
 class Type extends Component {
 	state = {
-		plants: [
-			(family_latin_name = ""),
-			(family_polish_name = ""),
-			(group_geohist = ""),
-			(group_raunkiaer = ""),
-			(id = 2),
-			(latin_name = ""),
-			(name_synonyms = ""),
-			(polish_name = ""),
-		],
+		family_latin_name: "",
+		family_polish_name: "",
+		group_geohist: "",
+		group_raunkiaer: "",
+		id: null,
+		latin_name: "",
+		name_synonyms: "",
+		polish_name: "",
 	};
 	handleGetTypes = e => {
 		e.preventDefault;
 		var get = () => {
 			const link = "https://kfsz.pythonanywhere.com/api/";
 			const axios = require("axios");
-			return axios
-				.get(`${link}plants/?`)
-				.then(response => {
-					console.log(response.data.results);
-					let plants2 = [];
-					response.data.results.map(id => {
-						return plants2.push();
-					});
-				})
-				.catch(error => {
-					console.log(error);
-				});
+			return (
+				axios
+					.get(`${link}plants/?`)
+					.then(response => {
+						// console.log(response.data.results);
+						let plants2 = [];
+						response.data.results.map(id => {
+							return plants2.push(id);
+						});
+						console.log(plants2);
+					})
+					// this.setState({
+					// 	this.state.family_latin_name = plants2.family_latin_name,
+					// })
+					.catch(error => {
+						console.log(error);
+					})
+			);
 		};
 		get();
 	};
